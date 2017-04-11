601e70f4a13bbe928872db65a838a9114ddeca77
Lorenzo Tait <lorenzo.tait95@gmail.com>
2017-04-11 11:07:51 +0200

    Add Char&Boolean

---
+++ JavaPrograms/CharAndBoolean/src/com/lorenzotait/Main.java
@@ -0,0 +1,15 @@
+package com.lorenzotait;
+
+public class Main {
+
+    public static void main(String[] args) {
+        char myChar = '\u00A9';
+        System.out.println(myChar);
+
+        boolean myBoolean = true;
+        boolean isMale = true;
+
+        char otherChar = '\u00AE';
+        System.out.println(otherChar);
+    }
+}
