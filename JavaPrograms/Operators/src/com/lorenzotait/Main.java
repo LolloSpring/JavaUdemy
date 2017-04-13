92bd17ec82dadaa5b2d652a2039906a69e2bddbf
Lorenzo Tait <lorenzo.tait95@gmail.com>
2017-04-13 16:56:17 +0200

    Add Operators part2 and finish section4

--- JavaPrograms/Operators/src/com/lorenzotait/Main.java
+++ JavaPrograms/Operators/src/com/lorenzotait/Main.java
@@ -58,9 +58,41 @@ public class Main {
         if (topScore >= 100)                // <= or >= or < or >
             System.out.println("Print the topScore " + topScore);
 
-        int secondScore = 60;
-        if (topScore > secondScore && topScore < 100)
+        int secondScore = 95;
+        if ((topScore > secondScore) && (topScore < 100))
             System.out.println("Greater than secondScore and less than 100");
+
+        if ((topScore > 90) || (secondScore <= 90))
+            System.out.println("One of these test is true");
+
+        int newValue = 50;
+        //if (newValue = 50)
+        //    System.out.println("This is an error");
+        if (newValue == 50)
+            System.out.println("This is true");
+
+        boolean isCar = false;
+        if (isCar = true)
+            System.out.println("This is not suppose to happen");
+        if (isCar)                  // this is MUCH BETTER
+            System.out.println("This is not suppose to happen");
+
+        isCar = true;
+        boolean wasCar = isCar ? true : false;
+        if (wasCar)
+            System.out.println("wasCar is true");
+
+        // CHALLENGE
+
+        double a = 20d;
+        double b = 80d;
+        double c = (a+b)*25d;
+        System.out.println(c);
+        double d = c % 40;
+        System.out.println(d);
+        if (d<=20)
+            System.out.println("Total was over the limit");
+
 
     }
 }
