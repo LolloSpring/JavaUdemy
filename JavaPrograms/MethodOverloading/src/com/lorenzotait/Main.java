1f437c0de9f312cb503e0c14b24476f0aebb7e3b
Lorenzo Tait <lorenzo.tait95@gmail.com>
2017-04-13 19:10:15 +0200

    Add Blocks Expression Method Overloading

---
+++ JavaPrograms/MethodOverloading/src/com/lorenzotait/Main.java
@@ -0,0 +1,21 @@
+package com.lorenzotait;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int newScore = calculateScore("Tim", 500);
+        System.out.println("newScore is " + newScore);
+        calculateScore(75);
+    }
+
+    public static int calculateScore (String playerName, int score){
+        System.out.println("Player " + playerName + " scored " + score + " points");
+        return score * 1000;
+    }
+
+    public static int calculateScore (int score){
+        System.out.println("Unnamed player scored " + score + " points");
+        return score * 1000;
+    }
+
+}
---
+++ JavaPrograms/StatementsWhiteSpacesAndIndenting/src/com/lorenzotait/Main.java
@@ -0,0 +1,18 @@
+package com.lorenzotait;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int myVariable = 50;
+        myVariable++;
+        myVariable--;
+        System.out.println("This is a test");
+
+        System.out.println("This is " +
+                    "another "+
+                    " still more ");
+
+        int anotherVariable = 5; myVariable--; //is still 2 statements in 1 line
+    }
+
+}
