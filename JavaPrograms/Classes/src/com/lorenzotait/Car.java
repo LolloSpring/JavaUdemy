5c3f62aa4f8fbcfc72b3cc53747a1ae18a857933
Lorenzo Tait <lorenzo.tait95@gmail.com>
2017-04-14 19:00:15 +0200

    Add creating Classes

---
+++ JavaPrograms/Classes/src/com/lorenzotait/Car.java
@@ -0,0 +1,22 @@
+package com.lorenzotait;
+
+/**
+ * Created by LorenzoTait on 14/04/17.
+ */
+public class Car {
+
+    private int doors;
+    private int wheels;
+    private String model;
+    private String engine;
+    private String colour;
+
+    public void setModel(String model){
+        this.model = model;
+    }
+
+    public String getModel(){
+        return this.model;
+    }
+
+}
