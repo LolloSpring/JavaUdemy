5c635ce4f5c04c0d011e31e44cf0fb4089ab539c
Lorenzo Tait <lorenzo.tait95@gmail.com>
2017-04-14 16:05:26 +0200

    Add end of section5

--- JavaPrograms/MethodOverloading/src/com/lorenzotait/Main.java
+++ JavaPrograms/MethodOverloading/src/com/lorenzotait/Main.java
@@ -6,6 +6,10 @@ public class Main {
         int newScore = calculateScore("Tim", 500);
         System.out.println("newScore is " + newScore);
         calculateScore(75);
+        calculateScore();
+        System.out.println("The result is = " + calcFeetAndInchesToCentimeters(-1,1));
+
+        System.out.println("The 2nd result is = " + calcFeetAndInchesToCentimeters(100));
     }
 
     public static int calculateScore (String playerName, int score){
@@ -18,4 +22,27 @@ public class Main {
         return score * 1000;
     }
 
+    public static int calculateScore (){
+        System.out.println("No score");
+        return 0;
+    }
+
+    public static double calcFeetAndInchesToCentimeters (double feet, double inches){
+        if ((feet>=0)&&((inches>=0)&&(inches<=12))){
+            return (((feet*12)+inches)*2.54);
+        } else {
+            return -1;
+        }
+    }
+
+    public static double calcFeetAndInchesToCentimeters (double inches){
+        if (inches>=0){
+            double feet = (int) inches / 12;
+            double remainingInches = (int) inches % 12;
+            System.out.printf(inches + " inches is equal to " + feet + " feet and remaining inches "+ remainingInches + " inches" );
+            return calcFeetAndInchesToCentimeters(feet, remainingInches);
+        } else {
+            return -1;
+        }
+    }
 }
